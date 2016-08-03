582ee6cb011a8fc9c2b8e0827e5a84115e1d9596
Sam <duo929@hotmail.com>
2016-08-03 20:08:10 +0200

    Add SQLite3 dependency, add simple test

--- src/test/Main.cpp
+++ src/test/Main.cpp
@@ -1,7 +1,14 @@
-
+#include "sqlite3.h"
 
 
 int main( int iArgc, char* pszArgV[] )
 {
+	sqlite3* pConnection;
+
+	if( SQLITE_OK == sqlite3_open( "file.sqlite", &pConnection ) )
+	{
+		sqlite3_close( pConnection );
+	}
+
 	return 0;
 }
