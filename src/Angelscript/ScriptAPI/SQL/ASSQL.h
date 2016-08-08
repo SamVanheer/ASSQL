e321c614ca7a2244bcf2476071d4e4076453aae7
Sam <duo929@hotmail.com>
2016-08-08 21:28:29 +0200

    Doc ASSQLLogFunction

--- src/Angelscript/ScriptAPI/SQL/ASSQL.h
+++ src/Angelscript/ScriptAPI/SQL/ASSQL.h
@@ -1,6 +1,9 @@
 #ifndef ANGELSCRIPT_SCRIPTAPI_SQL_ASSQL_H
 #define ANGELSCRIPT_SCRIPTAPI_SQL_ASSQL_H
 
+/**
+*	Log function pointer to use in SQL code.
+*/
 using ASSQLLogFunction = void ( * )( const char* const pszFormat, ... );
 
 #endif //ANGELSCRIPT_SCRIPTAPI_SQL_ASSQL_H
